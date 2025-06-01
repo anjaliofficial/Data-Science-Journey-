39e86f141bc0430e9e23c4035819d4ea9b5f717a
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-01 10:10:57 +0545

    Pipes

--- Pipes.R
+++ Pipes.R
@@ -21,4 +21,16 @@ options(scipen = 1000)
 A = filter(gapminder, continent=="Europe", year==2007) 
 B = select(A,-continent,-year) 
 ggplot(B,aes(x=gdpPercap,y=lifeExp, size=pop)) + 
-  geom_point(alpha=0.5)
+  geom_point(alpha=0.7) # standard protocols in R 0.7 nai rakha bhanchhaa
+
+# Nested function calls 
+ggplot(select(filter(gapminder, continent=="Europe", year==2007), 
+              -continent,-year),aes(x=gdpPercap,y=lifeExp, size=pop)) + 
+  geom_point(alpha=0.5) 
+
+Pipes 
+gapminder %>% 
+  filter(continent=="Europe", year==2007) %>% 
+  select(-continent,-year) %>% 
+  ggplot(aes(x=gdpPercap,y=lifeExp, size=pop)) + 
+  geom_point(alpha=0.5) 
