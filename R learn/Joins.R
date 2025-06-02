e5d24012baebab3eb9aeed67478fcb708ef5f41e
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-02 11:56:16 +0545

    question

--- R learn/Joins.R
+++ R learn/Joins.R
@@ -55,3 +55,48 @@ flights %>%
 
 
 #Finally we just want the full name of the airport. 
+
+flights %>% 
+  group_by(dest) %>% 
+  summarise(mean_delay=mean(arr_delay, na.rm=TRUE)) %>% 
+  filter(mean_delay==min(mean_delay, na.rm=TRUE)) %>% 
+  left_join(airports,by=c("dest"="faa")) %>% 
+  select(name) 
+
+
+
+
+
+#Exercise.  Use the nycflights13 package datasets to answer the following. 
+#(a) Considering only flights from JFK to SEA, what was the average, min, and max air 
+#time of those flights? 
+  
+
+library(dplyr)
+library(nycflights13)
+
+jfk_to_sea <- flights %>%
+  filter(origin == "JFK", dest == "SEA") %>%
+  summarise(
+    average_air_time = mean(air_time, na.rm = TRUE),
+    min_air_time = min(air_time, na.rm = TRUE),
+    max_air_time = max(air_time, na.rm = TRUE)
+  )
+
+print(jfk_to_sea)
+  
+#(b) Which city was flown to with the highest average speed? 
+
+library(nycflights13)
+library(tidverse)
+View(airports)
+
+flights %>% 
+  mutate(speed_mph = distance / (air_time / 60)) %>% 
+  group_by(dest) %>% 
+  summarise(avg_speed = mean (speed_mph, na.rm = TRUE)) %>% 
+  top_n(1, avg_speed) %>% 
+  left_join(airports, by = c("dest" = "faa")) %>% 
+  select(dest, name, avg_speed, tzone)
+
+  
