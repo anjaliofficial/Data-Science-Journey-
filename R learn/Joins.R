3cac190ced187a988b4ff1db8835243953af63ca
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-02 11:19:01 +0545

    qn1

--- R learn/Joins.R
+++ R learn/Joins.R
@@ -4,5 +4,14 @@ View(airlines)
 data(package= "nycflights13")
 View(flights)
 
+#Which airline has the highest number of delayed departures?
 
-
+delayed_flights = flights %>%       
+  # start with the flights 
+  filter(dep_delay>0) %>%           
+  # find only the delays 
+  group_by(carrier) %>%             
+  # group by airline (carrier) 
+  summarise(num_delay=n())          
+# count the observations 
+delayed_flights
