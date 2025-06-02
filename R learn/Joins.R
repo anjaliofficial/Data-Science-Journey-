590aac326012353a5893344ba04cf7e64c570588
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-02 11:21:50 +0545

    whole sequence as one dplyr pipe

--- R learn/Joins.R
+++ R learn/Joins.R
@@ -18,3 +18,12 @@ delayed_flights
 
 
 left_join(delayed_flights,airlines,by="carrier") 
+
+#It is possible to complete the whole sequence as one dplyr pipe. 
+flights %>%                             # start with the flights 
+  filter(dep_delay>0) %>%               # find only the delays 
+  group_by(carrier) %>%                 # group by airline (carrier) 
+  summarise(num_delay=n()) %>%          # count the observations 
+  left_join(airlines,by="carrier") %>%  # join by common key 
+  filter(num_delay==max(num_delay)) %>% # find most delayed 
+  select(name)                          # select the airline 
