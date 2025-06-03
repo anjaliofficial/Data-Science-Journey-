562488932842cea450bf81a54afcda6bce89d8d6
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-03 20:51:32 +0545

    excute it

--- Calculus/Gradient Descent.R
+++ Calculus/Gradient Descent.R
@@ -25,16 +25,16 @@ w_values[1] = w
 cost_values[1] = cost(w)
 for (i in 1:iterations){
   w = w-alpha*gradient(w) # move in opposite directions of the gradient 
-  w_value[i+1] = w # save u[pdated w]
+  w_values[i+1] = w # save u[pdated w]
   cost_values[i+1] = cost(w) # save cost at updated w 
   
 }
 
 # plot the cost function curve 
-curve(w^2, from = -6, to = 6, col = "blue", lwd = 2,
-      xlab = "w", ylab = "Cost J (W)", 
-      main = "Gradient Descent Visualization "
-      )
+curve(x^2, from = -6, to = 6, col = "blue", lwd = 2,
+      xlab = "w", ylab = "Cost J(w)", 
+      main = "Gradient Descent Visualization")
+
 
 #plot the stpes taken by gradient descent 
 points (w_values, cost_values, col= "red", pch = 19 ) # red dots 
@@ -44,3 +44,5 @@ lines(w_values, cost_values, col = "gray", lty = 2) # dashed lines showing path
 legend("topright", legend = c("Cost function", "GD steps"),
        col = c("blue", "red",lty = c(1, NA), pch = c(NA, 19))
        )
+
+
