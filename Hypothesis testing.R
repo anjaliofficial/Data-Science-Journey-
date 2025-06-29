e12909afb907242f98e12e947bac03619d7840ef
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-29 07:49:26 +0545

    not working

--- Hypothesis testing.R
+++ Hypothesis testing.R
@@ -7,3 +7,24 @@ library(ISLR)
 credit = as_tibble(Credit) 
 credit = select(credit,Balance,Limit,Income) 
 summary(credit)
+
+#(2) Let us take a look at the correlation between the variables (take them in pairs). 
+#install.packages("GGally")    
+# run only once 
+library(GGally)
+ggscatmat(select(credit,Balance,Limit,Income)) 
+
+
+#3) Suppose we are interested in predicting credit card balance (Balance) using credit 
+#card limit (Limit), i.e., the response variable is Balance and the predictor variable is 
+#Limit. 
+ggplot(credit, aes(x=Limit,y=Balance)) + 
+  geom_point() + 
+  geom_smooth(method="lm",se=FALSE) 
+model = lm(Balance~Limit, data=credit) 
+summary(model) 
+model$coefficients
+
+
+#Stii not working
+
